eeb766d3c598600cfe25d5d141492acc45380e46
Tritolomei <serpik4@gmail.com>
2019-03-27 11:03:39 +0200

    lab 2 init

--- lab1/laba2.js
+++ lab1/laba2.js
@@ -8,20 +8,24 @@ function convertData(str) {
     for (var i = 1; i < str.length; i++) {
         tmp = str[i].split(',');
         l = tmp.length - 1;
-
         for (var j = 1; j < l; j++) {
             tmp[j] = parseFloat(tmp[j]);            
         }
         tmp[l] = tmp[l] === 'TRUE';
-        arr[i - 1] = tmp;        
+        let output = {
+            name : tmp[0],
+            marksArray : tmp.slice(1,tmp.length - 2),
+            budget :  tmp[tmp.length - 1]
+        }
+        arr[i - 1] = output;        
     }
-    return arr;    
+    return arr;
 }
 
 function onlyBudget(arr) {
     var res = [];
     for (var i = 0; i < arr.length; i++) {
-        if (arr[i].pop()) {
+        if (arr[i].budget) {
             res.push(arr[i]);
         }
     }
@@ -33,14 +37,14 @@ students = onlyBudget(convertData(students.toString()));
 
 
 for (var i = 0; i < students.length; i++) {
-    students[i] = [
-        students[i][0],
-        avarege(students[i].slice(1))
-    ];
+    students[i] = {
+        name : students[i].name,
+        average : avarege(students[i].marksArray)
+    };
 }
 
 students.sort(function (a,b) {
-    return b[1] - a[1];    
+    return b.average - a.average;    
 });
 
 function avarege(arr) {
@@ -53,9 +57,9 @@ function avarege(arr) {
 }
 
 
-min = students[students.length - 1][1];
+min = students[students.length - 1].average;
 for (var i = 0; i < students.length; i++) {
-    res += students[i][0] + ',' + students[i][1].toFixed(3) + '\r\n';    
+    res += students[i].name + ',' + students[i].average.toFixed(3) + '\r\n';    
 }
 
 qtyS40 = Math.round(40 * students.length / 100);
